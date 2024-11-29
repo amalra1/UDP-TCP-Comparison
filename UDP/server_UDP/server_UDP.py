4d24d1101beb9e1d3f18f1752b9e04c3278e1677
Arthur Lima <ArthurSantos434@gmail.com>
2024-11-29 10:31:39 -0300

    Arquivos finalizados

--- UDP/server_UDP/server_UDP.py
+++ UDP/server_UDP/server_UDP.py
@@ -1,47 +1,46 @@
 import socket
 import os
 
-def send_file_to_client(filename):
-    HOST = "127.0.0.1"  # Endereço padrão de loopback (localhost)
-    PORT = 65433        # Porta para escutar (portas não privilegiadas são > 1023)
-    
+BUFFER_SIZE = 1024  # Tamanho do buffer
+ARCHIVE_NAME = 'socm-video-17.mkv'
+
+def send_file_to_client(filename, addr, s):
+
     # Obtém o tamanho do arquivo e calcula o número de pacotes
     file_size = os.path.getsize(filename)  
-    num_packets = (file_size // 1024) + 1  
+    num_packets = (file_size // BUFFER_SIZE) + 1
+        
+    sent_packets = 0  # Contador de pacotes enviados
+
+    # Lê o arquivo em pedaços de BUFFER_SIZE bytes
+    with open(filename, 'rb') as file:
+        while chunk := file.read(BUFFER_SIZE):  
+            # Envia cada pedaço do arquivo para o cliente
+            s.sendto(chunk, addr)
+            sent_packets += 1
+
+    # Envia uma mensagem final para indicar o fim da transferência
+    s.sendto(b'END', addr)
+
+    print(f"Arquivo {filename} enviado com sucesso.")
+    print(f"Tamanho total transferido: {file_size} bytes")
+    print(f"Total de pacotes enviados: {sent_packets}\n")
+
+if __name__ == '__main__':
+
+    HOST = "127.0.0.1"  # Endereço padrão de loopback (localhost)
+    PORT = 65433        # Porta para escutar (portas não privilegiadas são > 1023)
 
     # Cria socket UDP
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        # Associa o socket ao HOST e PORT especificados
         s.bind((HOST, PORT))
-        
-        # Espera por uma mensagem inicial do cliente para saber o endereço e porta
         print("Servidor esperando por conexões...")
-        data, addr = s.recvfrom(1024)
-        print(f"Conectado por {addr}")
-        
-        # Contador de pacotes enviados
-        sent_packets = 0  
-        
-        # Lê o arquivo em pedaços de 1024 bytes
-        with open(filename, 'rb') as file:
-            while chunk := file.read(1024):  
-                
-                # Envia cada pedaço do arquivo para o cliente
-                s.sendto(chunk, addr)
-                sent_packets += 1
-                
-        # Envia uma mensagem final para indicar o fim da transferência
-        s.sendto(b'END', addr)
-        
-        print(f"Arquivo {filename} enviado com sucesso.")
-        imprimir_resumo(file_size, sent_packets)
 
-def imprimir_resumo(tamanho_total, pacotes_enviados):
-    print(f"\nTamanho total transferido: {tamanho_total} bytes")
-    print(f"Total de pacotes enviados: {pacotes_enviados}")
-
-if __name__ == '__main__':
-    
-    # Caminho do arquivo a ser enviado
-    filename = 'UDP/arquivos_origem/FLS.rar'  
-    
-    send_file_to_client(filename)
+        while True:
+            # Espera por uma mensagem inicial do cliente
+            data, addr = s.recvfrom(BUFFER_SIZE)
+            if data == b'START':  # Verifica se a mensagem recebida é 'START'
+                print(f"Requisição de arquivo recebida de {addr}")
+                filename = f'./Arquivos_server_UDP/{ARCHIVE_NAME}'
+                send_file_to_client(filename, addr, s)
